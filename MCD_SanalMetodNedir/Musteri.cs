60a04eb99dcf84f5e4aa0463fef1d5726dbf66d9
jackup <yk.58@hotmail.com>
2021-12-03 17:01:59 +0300

    Add project files.

---
+++ MCD_SanalMetodNedir/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetodNedir
+{
+    public class Musteri // : Object => Framework standart olarak tüm nesneleri object'ten türetir.
+    {
+        public int musteriId { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+
+        public override string ToString()
+        {
+            return isim + "\t" + soyisim;
+        }
+    }
+}
---
+++ MCD_SanalMetodNedir/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetodNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Sanal Metot Nedir
+             * Sanal Metot dediğimiz baseclass içerisinde bulunan bir metotun istenirse türeyen sınıf içerisinde nasıl yeniden şekillenebileceğini inceleyeceğiz.
+             * 
+             * Kalıtım konusunda tüm nesnelerin object nesnesinden kalıtıldığını ve bazı metotların object nesnesinden bize geldiğini söylemiştik.
+             * Sanal metot anlatımına object içerisinde bize standart olarak gelen toStrin metotunu inceleyerek başlayacağız.
+             * Sanal metotlar, X-men filminde bulunan mystique karakterine benzetebiliriz.
+             * Ortama göre ken şeklini değiştirebilen bir karakter olması buna bir örnek olabilir.
+             * 
+             * Object => ToString();
+             * Musteri M1 = new Musteri();
+             * Object => Musteri
+             *           M1.ToString();
+             *           
+             * Namespace.Musteri
+             * 
+             * 
+             */
+
+            Musteri M1 = new Musteri();
+            M1.isim = "Faik";
+            M1.soyisim = "Turan";
+            string toStringMesaj = M1.ToString();
+            Console.WriteLine(toStringMesaj);
+
+            Console.ReadKey();
+
+            //MCD_SanalMetotNedir.Musteri
+        }
+    }
+}
